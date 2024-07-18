029c6ad87bfbb4546aed2615215e6b17ed65d41c
Ville Sirviö <VSirvio@users.noreply.github.com>
2024-07-18 22:55:24 +0300

    Lisää yksinkertainen tietokanta viestejä varten

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///tsoha"
+db = SQLAlchemy(app)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def message_list():
+    result = db.session.execute(text("SELECT content FROM messages"))
+    messages = result.fetchall()
+    return render_template("messages.html", count=len(messages), messages=messages)
