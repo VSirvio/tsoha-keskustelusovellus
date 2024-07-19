f15e7f4070a7b673bb6e31c5ea697d15dfedb9d5
Ville Sirviö <VSirvio@users.noreply.github.com>
2024-07-19 10:29:53 +0300

    Luo schema.sql

--- resetdb.sh
+++ resetdb.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source .env
 sudo -u postgres -- sh -c "dropdb $TSOHA_DATABASE_NAME; createdb $TSOHA_DATABASE_NAME; psql -d $TSOHA_DATABASE_NAME -c 'ALTER SCHEMA public OWNER TO $USER;'"
+psql -d $TSOHA_DATABASE_NAME < schema.sql
