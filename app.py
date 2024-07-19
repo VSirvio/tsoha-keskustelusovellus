33e704b5725b7c96fe027ae2938610cdb60a6093
Ville Sirviö <VSirvio@users.noreply.github.com>
2024-07-19 10:14:09 +0300

    Ota dotenv käyttöön

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///tsoha"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 @app.route("/")
