9e05ccc35d2bf3def59d2b05d76841dbcfac9e01
Ville Sirviö <VSirvio@users.noreply.github.com>
2024-08-18 16:43:12 +0300

    Remove type hints from config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
-from typing import Final, Literal
-
 # Uses PostgreSQL date/time formatting patterns for 'to_char(timestamp, text)'
-DATE_FORMAT : Final[str] = "DD.MM.YYYY klo HH24:MI"
+DATE_FORMAT = "DD.MM.YYYY klo HH24:MI"
 
-type OrderBy = Literal["newest", "oldest", "most_liked", "most_disliked"]
-DEFAULT_ORDER : Final[OrderBy] = "newest"
+# Possible values are "newest", "oldest", "most_liked", and "most_disliked"
+DEFAULT_ORDER = "newest"
