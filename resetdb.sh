87cefa898681639f344113d167453fa286ff9f8a
Ville Sirviö <VSirvio@users.noreply.github.com>
2024-08-18 11:06:48 +0300

    Run 'cd' command in the 'sudo' command in resetdb.sh to avoid the 'could not change directory' error messages

--- resetdb.sh
+++ resetdb.sh
@@ -3,7 +3,7 @@
 source .env
 
 # Delete the database and create it again to make sure it is empty. Change the relevant ownership to the current user, because by default the database cannot be modified by anyone else than 'postgres' user. (All these commands are run as 'postgres' user using sudo.)
-sudo -u postgres -- sh -c "dropdb $TSOHA_DATABASE_NAME; createdb $TSOHA_DATABASE_NAME; psql -d $TSOHA_DATABASE_NAME -c 'ALTER SCHEMA public OWNER TO $USER;'"
+sudo -u postgres -- sh -c "cd; dropdb $TSOHA_DATABASE_NAME; createdb $TSOHA_DATABASE_NAME; psql -d $TSOHA_DATABASE_NAME -c 'ALTER SCHEMA public OWNER TO $USER;'"
 
 psql -d $TSOHA_DATABASE_NAME < schema.sql
 
